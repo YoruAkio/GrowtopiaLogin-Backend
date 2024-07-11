97aee9f9d0afb795c9a36757c087649731970279
YoruAkio <yoruakio@proton.me>
2024-07-11 14:21:47 +0100

    feat: Implement GrowID login and validation
    Adds support for GrowID login and validation. Includes a new endpoint for validating login credentials and updating the dashboard with GrowID-specific features.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -11,6 +12,7 @@ app.use(function (req, res, next) {
     next();
 });
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // add logger on get or post
 app.use(function (req, res, next) {
@@ -24,6 +26,33 @@ app.post('/player/login/dashboard', function (req, res) {
 
 app.get('/player/login/dashboard', function (req, res) {
     res.sendFile(__dirname + '/public/html/dashboard.html');
+});
+
+app.use(express.json());
+
+app.post('/player/growid/login/validate', (req, res) => {
+    // Extracting data from the request body
+    const _token = req.body._token;
+    const growId = req.body.growId;
+    const password = req.body.password;
+
+    const token = Buffer.from(
+        `_token=${_token}&growId=${growId}&password=${password}`,
+    ).toString('base64');
+
+    console.log(`Received: GrowID - ${growId}`);
+
+    res.send(`{
+        status: 'success',
+        message: 'Account Verified!',
+        token: ${token},
+        url: '',
+        accountType: 'growtopia',
+    }`);
+});
+
+app.post('/player/validate/close', function (req, res) {
+    res.send('<script>window.close();</script>');
 });
 
 app.get('/', function (req, res) {
