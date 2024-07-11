79a9b1f5e11f90469c5be3b54a18a70cc69c21b4
YoruAkio <yoruakio@proton.me>
2024-07-11 16:50:19 +0100

    feat: Add body-parser for handling request bodies
    Adds body-parser middleware to the application to enable parsing of request bodies, primarily to support JSON requests. This allows for processing of data sent by clients in a more structured and predictable manner.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
