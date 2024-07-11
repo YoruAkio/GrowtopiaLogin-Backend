cf0c7c754f05d607701f6a58a4edb5283e1f15fd
YoruAkio <yoruakio@proton.me>
2024-07-11 16:16:19 +0100

    adding base project

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
-const bodyParser = require('body-parser');
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -11,39 +9,23 @@ app.use(function (req, res, next) {
     );
     next();
 });
-app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// add logger on get or post
 app.use(function (req, res, next) {
     console.log(req.method, req.url);
     next();
 });
 
-app.post('/player/login/dashboard', function (req, res) {
-    res.sendFile(__dirname + '/public/html/dashboard.html');
-});
-
-app.get('/player/login/dashboard', function (req, res) {
-    res.sendFile(__dirname + '/public/html/dashboard.html');
-});
-
 app.use(express.json());
 
+app.post('/player/login/dashboard', function (req, res) {
+    // redirect to path /player/growid/login/validate
+    res.redirect('/player/growid/login/validate');
+});
+
 app.post('/player/growid/login/validate', (req, res) => {
-    // Extracting data from the request body
-    const _token = req.body._token;
-    const growId = req.body.growId;
-    const password = req.body.password;
-
-    const token = Buffer.from(
-        `_token=${_token}&growId=${growId}&password=${password}`,
-    ).toString('base64');
-
-    console.log(`Received: GrowID - ${growId}`);
-
     res.send(
-        `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"}`,
+        `{"status":"success","message":"Account Validated.","token":"BE SUPER DAY PLEASE","url":"","accountType":"growtopia"}`,
     );
 });
 
