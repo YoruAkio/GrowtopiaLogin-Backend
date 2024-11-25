f41aa362a77c6fe796338d7d65124e3e8983df1a
YoruAkio <yoruakio@proton.me>
2024-11-25 12:22:12 +0700

    feat(fix): removing unsued code, updating dashboard interface, fixing some error that the modals might be removed on cleibt webview

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const rateLimiter = require('express-rate-limit');
 const compression = require('compression');
 
 app.use(compression({
-    level: 9,
+    level: 5,
     threshold: 0,
     filter: (req, res) => {
         if (req.headers['x-no-compression']) {
@@ -14,25 +14,30 @@ app.use(compression({
         return compression.filter(req, res);
     }
 }));
-
+app.set('view engine', 'ejs');
+app.set('trust proxy', 1);
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept',
     );
+    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url} - ${res.statusCode}`);
     next();
 });
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(function (req, res, next) {
-    console.log(req.method, req.url);
-    next();
-});
 app.use(express.json());
 app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
 
-app.post('/player/login/dashboard', (req, res) => {
-    res.sendFile(__dirname + '/public/html/dashboard.html');
+app.all('/player/login/dashboard', function (req, res) {
+    const tData = {};
+    try {
+        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
+        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
+        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
+    } catch (why) { console.log(`Warning: ${why}`); }
+
+    res.render(__dirname + '/public/html/dashboard.ejs', { data: tData });
 });
 
 app.all('/player/growid/login/validate', (req, res) => {
@@ -47,10 +52,6 @@ app.all('/player/growid/login/validate', (req, res) => {
     res.send(
         `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"}`,
     );
-});
-
-app.post('/player/validate/close', function (req, res) {
-    res.send('<script>window.close();</script>');
 });
 
 app.get('/', function (req, res) {
