2b6c49938807dce102d1ea30f5a638bbe9a6dadc
ReallBoyy <rahmanbariq@gmail.com>
2024-12-13 15:10:23 +0700

    Add checktoken endpoint

--- index.js
+++ index.js
@@ -53,7 +53,17 @@ app.all('/player/growid/login/validate', (req, res) => {
         `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"}`,
     );
 });
-
+app.all('/player/growid/checktoken', (req, res) => {
+    const refreshToken = req.body;
+    let data = {
+        status: "success",
+        message: "Account Validated",
+        token = refreshToken,
+        url: "",
+        accountType: "growtopia"
+    };
+    res.send(data);
+});
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
