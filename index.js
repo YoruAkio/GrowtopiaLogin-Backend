c568e8b4a1d8f0bbbd7292ffb1cbe6041970b0af
rin_matsuok <127410475+Fanny2712@users.noreply.github.com>
2025-02-09 11:08:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -29,64 +29,39 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
 
-// Player Login
 app.all('/player/login/dashboard', function (req, res) {
     const tData = {};
     try {
-        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); 
-        const uName = uData[0].split('|'); 
-        const uPass = uData[1].split('|');
-        for (let i = 0; i < uData.length - 1; i++) { 
-            const d = uData[i].split('|'); 
-            tData[d[0]] = d[1]; 
-        }
-        if (uName[1] && uPass[1]) { 
-            res.redirect('/player/growid/login/validate'); 
-        }
-    } catch (why) { 
-        console.log(`Warning: ${why}`); 
-    }
+        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
+        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
+        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
+    } catch (why) { console.log(`Warning: ${why}`); }
 
     res.render(__dirname + '/public/html/dashboard.ejs', { data: tData });
 });
 
 app.all('/player/growid/login/validate', (req, res) => {
-    // Parse JSON data
-    let growId = req.body.growId || '';  
-    const password = req.body.password || '';
+    const _token = req.body._token;
+    const growId = req.body.growId;
+    const password = req.body.password;
 
-    // If no growId provided, create a Guest Account
-    if (!growId) {
-        growId = 'Guest' + Math.floor(10000 + Math.random() * 90000);
-        console.log(`Auto-Generated Guest Account: ${growId}`);
-    }
-
-    const _token = req.body._token || 'defaultToken';
     const token = Buffer.from(
         `_token=${_token}&growId=${growId}&password=${password}`,
     ).toString('base64');
 
-    res.send({
-        status: "success",
-        message: "Account Validated.",
-        token: token,
-        url: "",
-        accountType: "growtopia",
-        username: growId 
-    });
+    res.send(
+        `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"}`,
+    );
 });
 
-// Redirect
 app.all('/player/*', function (req, res) {
     res.status(301).redirect('https://api.yoruakio.tech/player/' + req.path.slice(8));
 });
 
-// Home
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-// Server Start
 app.listen(5000, function () {
     console.log('Listening on port 5000');
 });
